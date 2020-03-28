b4f9aae13487b4d5cc5e13348f5edf0444408bb7
Abu-Bakar <xpox21@bk.ru>
2020-03-28 01:18:37 +0100

    Выполнил второе домашнее задание, пересоздал репозиторий и удалил все лишнее

---
+++ js/lesson2.js
@@ -0,0 +1,20 @@
+alert('второе домашнее задание по JavaScript')
+
+let money = 400,
+    income = 100,
+    addExpenses = 'Cтраховка на Машину, Бензин, Обед в кафе, Интернет',
+    deposit = 10 > 5,
+    mission = 10000,
+    period = 7 ;
+
+console.log( money, income, deposit );
+
+console.log( addExpenses.length );
+
+console.log('Период равен ' + period + ' месяцев.', 'Цель заработать ' + mission + ' долларов.');
+
+console.log( addExpenses.toLowerCase().split(' '));
+
+budgetDay = money / 30;
+
+console.log(budgetDay.toFixed(2));
